776c03e439e17873916a1a69b8ca28e1fc42c575
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 11:04:47 +0200

    Windows Event ForwardedEvents Log Export Upload

---
+++ Windows Event ForwardedEvents Log Export.ps1
@@ -0,0 +1,9 @@
+﻿$logName = "ForwardedEvents"
+$exportPath = "C:\Logs\events-ForwardedEvents.csv"
+
+$events = Get-WinEvent -LogName $logName -MaxEvents 1000
+
+$events | Select-Object TimeCreated, Id, LevelDisplayName, Message | Export-Csv -Path $exportPath -NoTypeInformation
+
+Write-Host "Event Log Export Complete"
+Write-Host "File saved at: $exportPath"
