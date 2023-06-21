05758ba2546ec276f4dff2c90687f560f540355c
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 10:19:22 +0200

    Windows Event Application Log Export.ps1 Upload

---
+++ Windows Event Application Log Export.ps1
@@ -0,0 +1,9 @@
+﻿$logName = "Application"
+$exportPath = "C:\Logs\events-application.csv"
+
+$events = Get-WinEvent -LogName $logName -MaxEvents 1000
+
+$events | Select-Object TimeCreated, Id, LevelDisplayName, Message | Export-Csv -Path $exportPath -NoTypeInformation
+
+Write-Host "Event Log Export Complete"
+Write-Host "File saved at: $exportPath"
