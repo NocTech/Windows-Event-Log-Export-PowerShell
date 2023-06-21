f5024500c6dc543dcbd04131c93dd671a5d8ae13
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 11:03:18 +0200

    Windows Event System Log Export Upload

---
+++ Windows Event System Log Export.ps1
@@ -0,0 +1,9 @@
+﻿$logName = "System"
+$exportPath = "C:\Logs\events-System.csv"
+
+$events = Get-WinEvent -LogName $logName -MaxEvents 1000
+
+$events | Select-Object TimeCreated, Id, LevelDisplayName, Message | Export-Csv -Path $exportPath -NoTypeInformation
+
+Write-Host "Event Log Export Complete"
+Write-Host "File saved at: $exportPath"
